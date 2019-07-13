24bd3e049af3e8d96fc260d78a82b8eec2210483
FinMacDov <fmackenziedover1@sheffield.ac.uk>
2019-07-13 17:28:26 +0100

    backing up work

--- DL_NLP/get_started_with_DL_NLP.py
+++ DL_NLP/get_started_with_DL_NLP.py
@@ -1,9 +1,22 @@
 # load data
 from nltk.tokenize import word_tokenize
-filename = '/home/fin/DL/DL_NLP/example_text.txt'
+
+# Code options
+write_output = False
+
+# filename = '/home/fin/DL/DL_NLP/example_text.txt'
+filename = '/home/fin/DL/DL_NLP/tale_of_two_cities.txt'
 file = open(filename, 'rt')
 text = file.read()
 file.close()
+
 # split into words
 tokens = word_tokenize(text)
-print(tokens)
+
+# Write new output
+fname = 'token_output.txt'
+
+if write_output is True:
+    with open(fname, 'w+') as f:
+        for item in tokens:
+            f.write("%s\n" % item)
